6545ae2f36990d82f69e99802d9368edfa605b31
aniyant <kumarsunny30066@gmail.com>
2022-07-15 11:14:27 +0530

    final

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,16 +1,34 @@
-from h11 import Data
 from housing.config.configuration import Configuration
 from housing.logger import logging
 from housing.exception import HousingException
 
-from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact, ModelEvaluationArtifact, ModelTrainerArtifact,ModelPusherArtifact
 from housing.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from housing.component.data_ingestion import DataIngestion
 from housing.component.data_validation import DataValidation
 from housing.component.data_transformation import DataTransformation
+from housing.component.model_trainer import ModelTrainer,HousingEstimatorModel
+from housing.component.model_evaluation import ModelEvaluation
+from housing.component.model_pusher import ModelPusher
 import os,sys
+from typing import List
+import pandas as pd
 
-class Pipeline:
+from multiprocessing import Process
+from threading import Thread
+import uuid
+from collections import namedtuple
+from datetime import datetime
+from housing.constant import EXPERIMENT_DIR_NAME,EXPERIMENT_FILE_NAME
+
+
+Experiment = namedtuple("Experiment", ["experiment_id", "initialization_timestamp", "artifact_time_stamp",
+                                       "running_status", "start_time", "stop_time", "execution_time", "message",
+                                       "experiment_file_path", "accuracy", "is_model_accepted"])
+
+class Pipeline(Thread):
+    experiment = Experiment = Experiment(*([None]) * 11)
+    experiment_file_path = None
 
     def __init__(self,config: Configuration = Configuration()) -> None:
         try:
@@ -45,17 +63,64 @@ class Pipeline:
         except Exception as e:
             raise HousingException(e,sys) from e
 
-    def start_model_trainer(self):
-        pass
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
+                                                data_transformation_artifact=data_transformation_artifact)
+            
+            return model_trainer.initiate_model_trainer()
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
-    def start_model_evaluation(self):
-        pass
+    def start_model_evaluation(self,data_ingestion_artifact:DataIngestionArtifact,
+                                    data_validation_artifact:DataValidationArtifact,
+                                    model_trainer_artifact:ModelTrainerArtifact,
+                                    ):
+        try:
+            model_evaluation = ModelEvaluation(model_evaluation_config=self.config.get_model_evaluation_config(),
+                                                    data_ingestion_artifact=data_ingestion_artifact,
+                                                    data_validation_artifact=data_validation_artifact,
+                                                    model_trainer_artifact=model_trainer_artifact
+                                                    ) 
+            return model_evaluation.initiate_model_evaluation()
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
-    def start_model_pusher(self):
-        pass
+    def start_model_pusher(self,model_evaluation_artifact:ModelEvaluationArtifact) -> ModelPusherArtifact:
+        try:
+            model_pusher = ModelPusher(model_pusher_config=self.config.get_model_pusher_config(),
+                                       model_evaluation_artifact=model_evaluation_artifact)
+
+            return model_pusher.initiate_model_pusher()
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
     def run_pipeline(self):
         try:
+            if Pipeline.experiment.running_status:
+                logging.info("Pipeline is already running.")
+                return Pipeline.experiment
+
+            logging.info("Pipeline starting")
+            experiment_id = str(uuid.uuid4())
+
+            Pipeline.experiment = Experiment(experiment_id=experiment_id,
+                                             initialization_timestamp=self.config.time_stamp,
+                                             artifact_time_stamp=self.config.time_stamp,
+                                             running_status=True,
+                                             start_time=datetime.now(),
+                                             stop_time=None,
+                                             execution_time=None,
+                                             experiment_file_path=Pipeline.experiment_file_path,
+                                             is_model_accepted=None,
+                                             message="Pipeline has been started",
+                                             accuracy=None,
+                                             )
+
+            logging.info(f"Pipeline experiment: {Pipeline.experiment}")
+            self.save_experiment()
+
             
             #data ingestion
             data_ingestion_artifact = self.start_data_ingestion()
@@ -67,6 +132,86 @@ class Pipeline:
             data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
                                                                     data_validation_artifact=data_validation_artifact)
 
-            
+            #model trainer
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+
+            #model evaluation
+            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                    data_validation_artifact=data_validation_artifact,
+                                                                    model_trainer_artifact=model_trainer_artifact)
+
+            if model_evaluation_artifact.is_model_accepted:
+                # model pusher
+                model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
+                logging.info(f"Model pusher artifact:{model_pusher_artifact}")
+            else:
+                logging.info("Trained model Rejected")
+            logging.info("Pipeline completed.")
+
+            stop_time = datetime.now()
+
+            Pipeline.experiment = Experiment(experiment_id=experiment_id,
+                                             initialization_timestamp=self.config.time_stamp,
+                                             artifact_time_stamp=self.config.time_stamp,
+                                             running_status=False,
+                                             start_time=Pipeline.experiment.start_time,
+                                             stop_time=stop_time,
+                                             execution_time=stop_time-Pipeline.experiment.start_time,
+                                             experiment_file_path=Pipeline.experiment_file_path,
+                                             is_model_accepted=model_evaluation_artifact.is_model_accepted,
+                                             accuracy=model_trainer_artifact.model_accuracy
+                                             )
+
+            logging.info(f"Pipeline experiment : {Pipeline.experiment}")
+            self.save_experiment()
+
         except Exception as e:
             raise HousingException(e,sys) from e
+
+    def run(self):
+        try:
+            self.run_pipeline()
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+    def save_experiment(self):
+        try:
+            if Pipeline.experiment.experiment_id is not None:
+                experiment = Pipeline.experiment
+                experiment_dict = experiment._asdict()
+                experiment_dict:dict = {key:[value] for key,value in experiment_dict.items()}
+
+                experiment_dict.update({
+                    "created_time_stamp" : [datetime.now()],
+                    "experiment_file_stamp" : [os.path.basename(Pipeline.experiment.experiment_file_path)]
+                })
+
+                experiment_report = pd.DataFrame(experiment_dict)
+
+                os.makedirs(os.path.dirname(Pipeline.experiment_file_path),exist_ok=True)
+
+                if os.path.exists(Pipeline.experiment_file_path):
+                    experiment_report.to_csv(Pipeline.experiment_file_path,index=False,header=False,mode="a")
+                else:
+                    experiment_report.to_csv(Pipeline.experiment_file_path,mode="w",index=False,header=True)
+
+            else:
+                print("First start experiment")
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+    @classmethod
+    def get_experiments_status(cls,limit:int=5) -> pd.DataFrame:
+        try:
+            if os.path.exists(Pipeline.experiment_file_path):
+                df = pd.read_csv(Pipeline.experiment_file_path)
+                limit = -1 * int(limit)
+
+                return df[limit:].drop(columns=["experiment_file_path", "initialization_timestamp"], axis=1)
+            else:
+                return pd.DataFrame()
+
+        except Exception as e:
+            raise HousingException(e, sys) from e
--- housing/util/util.py
+++ housing/util/util.py
@@ -1,3 +1,4 @@
+from cv2 import trace
 import yaml
 from housing.exception import HousingException
 import os,sys
@@ -5,6 +6,21 @@ import numpy as np
 import dill
 import pandas as pd
 from housing.constant import *
+
+def write_yaml_file(file_path:str,data:dict=None):
+    """
+    Create yaml file
+    file_path:str
+    data:dict
+    """
+
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"w") as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise HousingException(e,sys) from e
 
 def read_yaml_file(file_path:str)->dict:
     """
@@ -53,6 +69,16 @@ def save_object(file_path:str,obj):
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise HousingException(e,sys) from e
+
+def load_object(file_path:str):
+    """
+    file_path: str
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise HousingException(e,sys) from e
 
